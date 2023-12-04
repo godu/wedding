0d83ba5ba5c8c34352ab5361af57d9cd6754aede
godu <arthur.weber@viacesi.fr>
2023-12-04 14:08:58 +0100

    Adds Cartouche

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,5 +1,5 @@
 
-import "./Background.css"
+import styles from "./Background.module.css"
 
 export interface Props { }
 
@@ -10,23 +10,23 @@ export const Background: React.FC<Props> = () => {
       <defs>
         <pattern id="Palmes" x="0" y="0" width="101" height="101" patternUnits="userSpaceOnUse">
           <g xmlns="http://www.w3.org/2000/svg">
-            <rect className={"cls-2"} x=".5" y=".5" width="100" height="100" />
-            <path className={"cls-3"} d="m80.5.5c0,19.5-.5,20.5-20,40,19.5-19.5,20.5-20,40-20V.5h-20Z" />
-            <line className={"cls-1"} x1="60.5" y1="40.5" x2=".5" y2="100.5" />
-            <path className={"cls-1"} d="m.5,100.5C20,81,20.5,80,20.5.5" />
-            <path className={"cls-1"} d="m40.5.5c0,59.5-.39,60.5-20,80" />
-            <path className={"cls-1"} d="m60.5.5c0,39.5-.5,40.5-20,60" />
-            <path className={"cls-1"} d="m80.5.5c0,19.5-.5,20.5-20,40" />
-            <path className={"cls-1"} d="m.5,100.5c19.5-19.5,20.5-20,100-20" />
-            <path className={"cls-1"} d="m100.5,60.5c-59.5,0-60.5.39-80,20" />
-            <path className={"cls-1"} d="m100.5,40.5c-39.5,0-40.5.5-60,20" />
-            <path className={"cls-1"} d="m100.5,20.5c-19.5,0-20.5.5-40,20" />
-            <rect className={"cls-1"} x=".5" y=".5" width="100" height="100" />
+            <rect className={styles["cls-2"]} x=".5" y=".5" width="100" height="100" />
+            <path className={styles["cls-3"]} d="m80.5.5c0,19.5-.5,20.5-20,40,19.5-19.5,20.5-20,40-20V.5h-20Z" />
+            <line className={styles["cls-1"]} x1="60.5" y1="40.5" x2=".5" y2="100.5" />
+            <path className={styles["cls-1"]} d="m.5,100.5C20,81,20.5,80,20.5.5" />
+            <path className={styles["cls-1"]} d="m40.5.5c0,59.5-.39,60.5-20,80" />
+            <path className={styles["cls-1"]} d="m60.5.5c0,39.5-.5,40.5-20,60" />
+            <path className={styles["cls-1"]} d="m80.5.5c0,19.5-.5,20.5-20,40" />
+            <path className={styles["cls-1"]} d="m.5,100.5c19.5-19.5,20.5-20,100-20" />
+            <path className={styles["cls-1"]} d="m100.5,60.5c-59.5,0-60.5.39-80,20" />
+            <path className={styles["cls-1"]} d="m100.5,40.5c-39.5,0-40.5.5-60,20" />
+            <path className={styles["cls-1"]} d="m100.5,20.5c-19.5,0-20.5.5-40,20" />
+            <rect className={styles["cls-1"]} x=".5" y=".5" width="100" height="100" />
           </g>
         </pattern>
         <pattern id="Palmes2" patternTransform="rotate(315) scale(.05 .05)" xlinkHref="#Palmes" xmlnsXlink="http://www.w3.org/1999/xlink" />
       </defs>
-      <rect className="cls-5" width="101" height="101" />
+      <rect className={styles["cls-5"]} width="101" height="101" />
     </svg>
   );
 
